5674125b5eb9714ec90da67cbe40af98f022a600
the-intern <ostclasses@gmail.com>
2024-11-19 14:03:14 -0600

    changes to lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "phelipetls/vim-hugo" },
     -- import/override with your plugins
     { import = "plugins" },
   },
